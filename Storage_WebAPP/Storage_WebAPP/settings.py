4b11f845e3cbb361dc3f1852bd432d5488510a71
danprav04 <62667601+danprav04@users.noreply.github.com>
2023-09-25 04:44:37 +0300

    superuser

--- Storage_WebAPP/Storage_WebAPP/settings.py
+++ Storage_WebAPP/Storage_WebAPP/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-$#tc1r)m5j876-ky!$bh!p%27%^%x*361jolx-7!zv37zbppt&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+# ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
