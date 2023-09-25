a6fbc3342fcac819b2aa6edc4073f133fe4e58d7
danprav04 <62667601+danprav04@users.noreply.github.com>
2023-09-25 05:33:52 +0300

    static try

--- Storage_WebAPP/Storage_WebAPP/settings.py
+++ Storage_WebAPP/Storage_WebAPP/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$#tc1r)m5j876-ky!$bh!p%27%^%x*361jolx-7!zv37zbppt&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['.vercel.app']
 ALLOWED_HOSTS = ['*']
@@ -116,8 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR), 'static']
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
